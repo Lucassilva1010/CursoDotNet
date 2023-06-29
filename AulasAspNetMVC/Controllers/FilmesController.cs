a6e104ac7b03868564b1060f987ba8058561430f
Lucas Augusto <lucas_lapada@hotmail.com>
2023-06-29 16:26:34 -0300

    Aula - 01 Rotas e Controllers

---
+++ AulasAspNetMVC/Controllers/FilmesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AulasAspNetMVC.Controllers
+{
+    public class FilmesController : Controller
+    {
+        public string Index()
+        {
+            return "Essa é minha action padrdão vindo da Controllers";
+        }
+        public string Benvindo(string nome, int numero) => $"Criando a minhas propria Actions {nome} - {numero} ";
+    }
+}

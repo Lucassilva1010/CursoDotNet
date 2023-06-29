859e68140ee76df3ef33558bbe9b8ff04dde0a08
Lucas Augusto <lucas_lapada@hotmail.com>
2023-06-29 16:08:47 -0300

    Inicio das Aulas do MVC

---
+++ AulasAspNetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AulasAspNetMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
